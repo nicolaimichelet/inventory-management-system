c60fa3b7b550b918afaf8b9fe1a0b1aafee8b3fa
drot <tordaustad@gmail.com>
2018-03-06 15:47:16 +0100

    Conflict

--- src/main/java/org/sims/controller/ServiceController.java
+++ src/main/java/org/sims/controller/ServiceController.java
@@ -1,12 +1,9 @@
 package org.sims.controller;
 
 
+import org.sims.exception.ResourceNotFoundException;
 import org.sims.model.Service;
 import org.sims.repository.ServiceRepository;
-import org.sims.exception.ResourceNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,35 +23,18 @@ public class ServiceController {
         return serviceRepository.findAll();
     }
 
-
-    // Get a single Service
-    @GetMapping("/services/{id}")
-    public ResponseEntity<Service> getServiceById(@PathVariable(value = "id") Long serviceId) {
-
-        Service service = serviceRepository.findById(serviceId)
-                .orElseThrow(() -> new ResourceNotFoundException("Service", "id", serviceId));;
-
-        if(service == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity.ok().body(service);
-    }
-
     @PostMapping("/services")
     public Service createService(@Valid @RequestBody Service service) {
         return serviceRepository.save(service);
     }
 
-    // Delete a service
-    @DeleteMapping("/services/{id}")
-    public ResponseEntity<?> deleteService(@PathVariable(value = "id") Long serviceId) {
-        Service service = serviceRepository.findById(serviceId)
-                .orElseThrow(() -> new ResourceNotFoundException("Service", "id", serviceId));
-        serviceRepository.delete(service);
-
-        return ResponseEntity.ok().build();
+    // Get a Single Service
+    @GetMapping("/notes/{id}")
+    public Service getServiceById(@PathVariable(value = "id") Long serviceId) {
+        return serviceRepository.findById(serviceId)
+                .orElseThrow(() -> new ResourceNotFoundException("Note", "id", serviceId));
     }
+
 
 
 }
