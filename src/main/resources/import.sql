4848a749b3eed1b40bcead892910b73bb8d5e82d
drot <tordaustad@gmail.com>
2018-03-28 21:24:37 +0200

    GET, POST, DELETE now works properly. Some cleanup.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,16 +1,58 @@
+-- noinspection SqlNoDataSourceInspectionForFile
 
--- INSERT INTO places (id, href, role) VALUES (1, 'http://www.google.com/', 'USA');
+INSERT INTO SERVICE_SPECIFICATION (dbid, href, id, name, version) VALUES (1, 'href1', 'id1', 'name1', 'version1');
+INSERT INTO SERVICE_SPECIFICATION (dbid, href, id, name, version) VALUES (2, 'href2', 'id2', 'name2', 'version2');
 
--- INSERT INTO servicecharacteristics (id, name, value) VALUES (1, 'speed', '16M');
+INSERT INTO SERVICE_ORDER (dbid, href, id) VALUES (1, 'href1', 'id1');
+INSERT INTO SERVICE_ORDER (dbid, href, id) VALUES (2, 'href2', 'id2');
 
--- INSERT INTO servicerelationship (id, type) VALUES (1, 'contains');
+INSERT INTO SERVICE (dbid, category, description, end_date, has_started, href, id, is_service_enabled, is_stateful, name, order_date, start_date, start_mode, state, type, service_order_dbid, service_specification_dbid) VALUES (1, 'category1', 'description1', 'endDate1', true, 'href1', 'id1', true, true, 'name1', 'orderDate1', 'startDate1', 'startMode1', 'state1', 'type1', 2, 1);
+INSERT INTO SERVICE (dbid, category, description, end_date, has_started, href, id, is_service_enabled, is_stateful, name, order_date, start_date, start_mode, state, type, service_order_dbid, service_specification_dbid) VALUES (2, 'category2', 'description2', 'endDate2', false, 'href2', 'id2', false, false, 'name2', 'orderDate2', 'startDate2', 'startMode2', 'state2', 'type2', 1, 2);
 
--- INSERT INTO serviceref (id, href, servicerelationship_id) VALUES (44, 'http://server:port/serviceInventoryManagement/service/44', 1);
+INSERT INTO RELATED_PARTY (dbid, href, id, name, role, valid_for) VALUES (1, 'href1', 'id1', 'name1', 'role1', 'validFor1');
+INSERT INTO RELATED_PARTY (dbid, href, id, name, role, valid_for) VALUES (2, 'href2', 'id2', 'name2', 'role2', 'validFor2');
 
--- INSERT INTO supportingservices (category, href, name) VALUES (59, 'http://server:port/serviceManagement/service/59', 'serviceName');
+INSERT INTO SERVICE_RELATED_PARTY (service_dbid, related_party_dbid) VALUES (1, 1);
+INSERT INTO SERVICE_RELATED_PARTY (service_dbid, related_party_dbid) VALUES (1, 2);
+INSERT INTO SERVICE_RELATED_PARTY (service_dbid, related_party_dbid) VALUES (2, 2);
 
--- INSERT INTO services (id, href, category, name, description, is_service_enabled, has_started, start_mode, is_stateful, state, place_id) VALUES (42, 'http://server:port/', 'CFS', 'asd', 'Description of Broadband service', true, true, '0', false, 'active', 1);
+INSERT INTO PLACE (DBID, HREF, ROLE, SERVICE_dbID) VALUES (1, 'HREF1', 'ROLE1', 1);
+INSERT INTO PLACE (DBID, HREF, ROLE, SERVICE_dbID) VALUES (2, 'HREF2', 'ROLE2', 1);
+INSERT INTO PLACE (DBID, HREF, ROLE, SERVICE_dbID) VALUES (3, 'HREF3', 'ROLE3', 2);
 
--- INSERT INTO service_supportingservices (service_id, supportingservice_id) VALUES (42, 59);
+INSERT INTO NOTE (DBID, author, date, text, SERVICE_dbID) VALUES (1, 'author1', 'date1','text1', 1);
+INSERT INTO NOTE (DBID, author, date, text, SERVICE_dbID) VALUES (2, 'author2', 'date2','text2', 1);
+INSERT INTO NOTE (DBID, author, date, text, SERVICE_dbID) VALUES (3, 'author3', 'date3','text3', 2);
 
--- INSERT INTO servicespecificationrefs (id, href, name, version, service_id) VALUES (4, 'http://server:port/serviceCatalogManagement/serviceSpecification/4', 'name', 'version', 42);
+INSERT INTO SERVICE_CHARACTERISTIC (dbID, NAME, VALUE, SERVICE_DBID) VALUES (1, 'name1', 'value1', 1);
+INSERT INTO SERVICE_CHARACTERISTIC (dbID, NAME, VALUE, SERVICE_DBID) VALUES (2, 'name2', 'value2', 2);
+INSERT INTO SERVICE_CHARACTERISTIC (dbID, NAME, VALUE, SERVICE_DBID) VALUES (3, 'name3', 'value3', 2);
+
+INSERT INTO SERVICE_REF (dbid, href, id) VALUES (1, 'href1', 'id1');
+INSERT INTO SERVICE_REF (dbid, href, id) VALUES (2, 'href2', 'id2');
+INSERT INTO SERVICE_REF (dbid, href, id) VALUES (3, 'href3', 'id3');
+
+INSERT INTO SERVICE_RELATIONSHIP (dbID, TYPE, SERVICE_DBID, SERVICE_REF_DBID) VALUES (1, 'type1', 1, 1);
+INSERT INTO SERVICE_RELATIONSHIP (dbID, TYPE, SERVICE_DBID, SERVICE_REF_DBID) VALUES (2, 'type2', 2, 2);
+INSERT INTO SERVICE_RELATIONSHIP (dbID, TYPE, SERVICE_DBID, SERVICE_REF_DBID) VALUES (3, 'type3', 1, 3);
+
+INSERT INTO SUPPORTING_RESOURCE (dbid, href, id, name) VALUES (1, 'href1', 'id1', 'name1');
+INSERT INTO SUPPORTING_RESOURCE (dbid, href, id, name) VALUES (2, 'href2', 'id2', 'name2');
+INSERT INTO SUPPORTING_RESOURCE (dbid, href, id, name) VALUES (3, 'href3', 'id3', 'name3');
+
+INSERT INTO SERVICE_SUPPORTING_RESOURCE (SERVICE_DBID, SUPPORTING_RESOURCE_DBID) VALUES (1, 2);
+INSERT INTO SERVICE_SUPPORTING_RESOURCE (SERVICE_DBID, SUPPORTING_RESOURCE_DBID) VALUES (2, 2);
+INSERT INTO SERVICE_SUPPORTING_RESOURCE (SERVICE_DBID, SUPPORTING_RESOURCE_DBID) VALUES (1, 3);
+INSERT INTO SERVICE_SUPPORTING_RESOURCE (SERVICE_DBID, SUPPORTING_RESOURCE_DBID) VALUES (1, 1);
+INSERT INTO SERVICE_SUPPORTING_RESOURCE (SERVICE_DBID, SUPPORTING_RESOURCE_DBID) VALUES (2, 1);
+
+INSERT INTO SUPPORTING_SERVICE (dbid, category, href, id, name) VALUES (1, 'category1', 'href1', 'id1', 'name1');
+INSERT INTO SUPPORTING_SERVICE (dbid, category, href, id, name) VALUES (2, 'category2', 'href2', 'id2', 'name2');
+INSERT INTO SUPPORTING_SERVICE (dbid, category, href, id, name) VALUES (3, 'category3', 'href3', 'id3', 'name3');
+
+INSERT INTO SERVICE_SUPPORTING_SERVICE (SERVICE_DBID, SUPPORTING_SERVICE_DBID) VALUES (1, 2);
+INSERT INTO SERVICE_SUPPORTING_SERVICE (SERVICE_DBID, SUPPORTING_SERVICE_DBID) VALUES (2, 2);
+INSERT INTO SERVICE_SUPPORTING_SERVICE (SERVICE_DBID, SUPPORTING_SERVICE_DBID) VALUES (1, 3);
+INSERT INTO SERVICE_SUPPORTING_SERVICE (SERVICE_DBID, SUPPORTING_SERVICE_DBID) VALUES (1, 1);
+INSERT INTO SERVICE_SUPPORTING_SERVICE (SERVICE_DBID, SUPPORTING_SERVICE_DBID) VALUES (2, 1);
+
